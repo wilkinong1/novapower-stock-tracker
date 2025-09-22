5751b7fffa60b78033004c5c8b0be19cb478ef32
wilkinong1 <wilkin.ong@gmail.com>
2025-09-22 16:32:20 +0800

    Update app_data.py

--- app_data.py
+++ app_data.py
@@ -11,7 +11,7 @@ import json
 bucket_name = "nova-power-cloud-storage-buckett"
 def get_file_gcs(bucket_name, blob_name):
     # client = storage.Client.from_service_account_info(gcp_credentials)
-    client = storage.Client.from_service_account_file("gcp_key.json")
+    client = storage.Client.from_service_account_json("gcp_key.json")
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
@@ -45,6 +45,7 @@ stock_tracker = last_purchase.merge(with_incoming, how='left', on=['item_name'])
 
 stock_tracker['check_stock'] = np.where(stock_tracker['available_stock'] < stock_tracker['purchase_last_60'], True, False)
 stock_tracker['check_stock_soft'] = np.where((stock_tracker['available_stock'] + stock_tracker['incoming_quantity']) < stock_tracker['purchase_last_60'], True, False)
+
 
 
 
